a5bf06251be6d491197c1298beca7b268532d74f
kishan <kishanknows@gmail.com>
2024-06-08 09:45:26 +0530

    updated top anime api and component

--- frontend/api/top-anime.ts
+++ frontend/api/top-anime.ts
@@ -2,18 +2,35 @@ import axios from 'axios';
 
 const baseUrl = 'https://api.jikan.moe/v4';
 
-export async function topAnimeApi() {
-  try {
-    const response = await axios({
-      method: 'get',
-      baseURL: baseUrl,
-      url: '/top/anime',
-      params: {
-        filter: 'airing',
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error);
+export type Filter = 'airing' | 'upcoming' | 'bypopularity' | 'favorite';
+export interface TopAnimeApiParams {
+  filter: Filter;
+}
+
+const RETRY_COUNT = 5;
+const RETRY_DELAY_MS = 100;
+
+function sleep(delay: number) {
+  return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+export async function topAnimeApi(params: TopAnimeApiParams) {
+  let count = RETRY_COUNT;
+  while (count > 0) {
+    try {
+      const response = await axios({
+        method: 'get',
+        baseURL: baseUrl,
+        url: '/top/anime',
+        params: params,
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      await sleep(RETRY_DELAY_MS);
+    } finally {
+      count -= 1;
+    }
   }
+  throw new Error(`Too many retries`);
 }
--- frontend/src/redux/slices/topAnimeSlice.ts
+++ frontend/src/redux/slices/topAnimeSlice.ts
@@ -1,15 +1,16 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import {Anime} from './suggestionSlice';
-import {topAnimeApi} from '../../../api';
+import {Filter, topAnimeApi} from '../../../api';
 
 export const getTopAnime = createAsyncThunk(
   'recommendation/getTopAnime',
-  async () => {
-    const response = await topAnimeApi();
+  async (filter: Filter) => {
+    const response = await topAnimeApi({filter: filter});
     const data = response?.data;
     const animeList: Anime[] = [];
     data.map((item: any) => {
       animeList.push({
+        id: item.mal_id,
         title: item.title,
         trailer_url: item.trailer.url,
         img_url: item.images.jpg.large_image_url,
@@ -17,16 +18,22 @@ export const getTopAnime = createAsyncThunk(
         rating: item.rating,
         duration: item.duration,
         score: item.score,
+        synopsis: item.synopsis,
       });
     });
-    return animeList;
+    return {filter, animeList};
   },
 );
 
 const topAnimeSlice = createSlice({
   name: 'recommendation',
   initialState: {
-    topAnime: [] as Anime[],
+    topAnime: {
+      airing: [] as Anime[],
+      upcoming: [] as Anime[],
+      bypopularity: [] as Anime[],
+      favorite: [] as Anime[],
+    },
     loading: false,
   },
   reducers: {},
@@ -35,7 +42,8 @@ const topAnimeSlice = createSlice({
       state.loading = true;
     });
     builder.addCase(getTopAnime.fulfilled, (state, action) => {
-      state.topAnime = action.payload;
+      const {filter, animeList} = action.payload;
+      state.topAnime[filter] = animeList;
       state.loading = false;
     });
     builder.addCase(getTopAnime.rejected, state => {
