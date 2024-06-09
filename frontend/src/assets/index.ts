83f72160dff02a2d5a2c3214e0ce33ef7c67e3c2
kishan <kishanknows@gmail.com>
2024-06-09 08:41:10 +0530

    added new icons

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,4 +1,11 @@
 export const ICONS = {
   search: require('./search.png'),
   back: require('./back.png'),
+  profile: require('./profile-filled.png'),
+  home: require('./home.png'),
+  discover: require('./discover.png'),
+  news: require('./news.png'),
+  homeFilled: require('./home-filled.png'),
+  discoverFilled: require('./discover-filled.png'),
+  newsFilled: require('./news-filled.png'),
 };
