48bc2b7a063ec5cd65a4bb8f18856328ad3142b5
kishan <kishanknows@gmail.com>
2024-06-13 09:51:21 +0530

    added news controller

---
+++ backend/src/controllers/newsController.ts
@@ -0,0 +1,31 @@
+import { dataSource } from "../config/db";
+import { News } from "../entities/News";
+import { NewsScraper } from "../scraper";
+import { Request, Response } from "express";
+
+export async function updateNews(req: Request, res: Response) {
+  const scraper = new NewsScraper();
+  const newsList = await scraper.scrapeNews(
+    "https://www.animenewsnetwork.com/news/"
+  );
+  try {
+    const newsRepository = dataSource.getRepository(News);
+    await newsRepository.clear();
+    await newsRepository.save(newsList);
+    return res.send("news updated");
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("Failed to update news");
+  }
+}
+
+export async function getNews(req: Request, res: Response) {
+  try {
+    const newsRepository = dataSource.getRepository(News);
+    const response = await newsRepository.find();
+    return res.send(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).send("Not found");
+  }
+}
