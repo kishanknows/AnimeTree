9a5cbae16687e9f73d96f1823079a57b6641c724
kishan <kishanknows@gmail.com>
2024-06-13 09:50:54 +0530

    updated db configuration

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { News } from "../entities/News";
 
 const port = process.env.DB_PORT as number | undefined;
 
@@ -11,7 +12,7 @@ export const dataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-
-  entities: [`${__dirname}/**/entities/*.{ts, js}`],
-  migrations: [`${__dirname}/**/migrations/*.{ts, js}`],
+  entities: [News],
+  synchronize: true,
+  logging: false,
 });
