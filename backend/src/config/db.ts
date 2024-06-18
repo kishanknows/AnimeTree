52fcceecacaacfabfbc3aebf3772dbdaf282f74b
kishan <kishanknows@gmail.com>
2024-06-18 09:49:29 +0530

    added user entity

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { News } from "../entities/News";
+import { User } from "../entities/Users";
 
 const port = process.env.DB_PORT as number | undefined;
 
@@ -12,7 +13,7 @@ export const dataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [News],
+  entities: [News, User],
   synchronize: true,
   logging: false,
 });
