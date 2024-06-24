900236f38fa942c76abff39939e75edeeb354407
kishan <kishanknows@gmail.com>
2024-06-24 13:28:03 +0530

    minor fix

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,9 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
-  createMigrate,
   FLUSH,
-  MigrationManifest,
   PAUSE,
   PERSIST,
   persistReducer,
@@ -13,22 +11,12 @@ import {
   REHYDRATE,
 } from 'redux-persist';
 import {rootReducer} from './reducers';
-import {User} from './slices/userSlice';
-
-// const migrations: MigrationManifest = {
-//   1: state => {
-//     return {
-//       ...state,
-//     };
-//   },
-// };
 
 const persistConfig = {
   key: 'root',
   version: 1,
   storage: AsyncStorage,
   whitelist: ['user'],
-  // migrate: createMigrate(migrations, {debug: false}),
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
